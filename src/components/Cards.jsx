03a3919b4d40ed496b4ea5412a7ea9d8aa81859e
Raghu2407 <raghoothamkalkeri96@gmail.com>
2025-07-24 12:05:28 +0530

    mobile friendly css

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-const Cards = ({ products: { id, name, image, short_desc , price } }) => {
+const Cards = ({ products: { id, name, image, short_desc, price } }) => {
   return (
     <div
       key={id}
@@ -19,14 +19,14 @@ const Cards = ({ products: { id, name, image, short_desc , price } }) => {
       </div>
       <div className="px-6 py-4 border-t border-gray-200 flex items-center justify-between">
         <span className="text-2xl font-bold text-gray-900 ">
-        ₹{price.toFixed(2)}
+          ₹{price.toFixed(2)}
         </span>
         <Link to={`/product/${id}`}>
-        <button
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Add to cart
-        </button>
+          <button
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Add to cart
+          </button>
         </Link>
       </div>
     </div>
